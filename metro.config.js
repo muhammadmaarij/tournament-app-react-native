fccb2bf8cdcda2ea6fc115a6d21756110aef2997
Muhammad Maarij <muhammadmaarij7@gmail.com>
2023-11-25 11:04:17 +0500

    first

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,17 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+module.exports = async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  const customConfig = {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+    },
+  };
+
+  return mergeConfig(defaultConfig, customConfig);
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {View, Dimensions} from 'react-native';
 import {Appbar} from 'react-native-paper';
+import GreenHeader from '../assets/svgs/GreenHeader.svg';
+
+const {width, height} = Dimensions.get('window');
 
 const Header = () => (
-  <Appbar.Header>
-    <Appbar.Content title="Welcome Hassan!" subtitle="Let's get started" />
-  </Appbar.Header>
+  <View>
+    <GreenHeader width={width} height={width*0.55} />
+    <Appbar.Header>
+      <Appbar.Content title="Welcome Hassan!" subtitle="Let's get started" />
+    </Appbar.Header>
+  </View>
 );
 
 export default Header;
